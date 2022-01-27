b51dbd6e1de7df78622acf85a107384b3602b908
cdaii <chakibdaii1995@gmail.com>
2022-01-27 20:44:17 +0000

    cdaii - project files

---
+++ src/main/groovy/com/example/samplegroovyproject/SampleGroovyProjectApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.samplegroovyproject
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SampleGroovyProjectApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SampleGroovyProjectApplication, args
+	}
+
+}
