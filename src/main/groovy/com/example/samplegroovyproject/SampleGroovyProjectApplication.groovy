29b0390677c5a295691b0ef5378e8312fb4f1e1d
cdaii <chakibdaii1995@gmail.com>
2022-02-02 16:02:20 +0000

    cdaii - updated properties and sanitization logic for k8s added

--- src/main/groovy/com/example/samplegroovyproject/SampleGroovyProjectApplication.groovy
+++ src/main/groovy/com/example/samplegroovyproject/SampleGroovyProjectApplication.groovy
@@ -1,13 +1,26 @@
 package com.example.samplegroovyproject
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.endpoint.SanitizableData
+import org.springframework.boot.actuate.endpoint.SanitizingFunction
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.env.CommandLinePropertySource
 
 @SpringBootApplication
 class SampleGroovyProjectApplication {
 
+	@Bean
+	SanitizingFunction sanitizingFunction(){
+		return new SanitizingFunction() {
+			@Override
+			SanitizableData apply(SanitizableData data) {
+				return data.getPropertySource().getName() == CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME ?
+						data.withValue("not gonna get it") : data;
+			}
+		}
+	}
 	static void main(String[] args) {
 		SpringApplication.run SampleGroovyProjectApplication, args
 	}
-
 }
