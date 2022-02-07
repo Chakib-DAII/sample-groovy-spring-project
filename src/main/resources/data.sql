03077a8e22cc6b996b3ea79c230d86bc8f394cd5
cdaii <chakibdaii1995@gmail.com>
2022-02-07 16:15:32 +0000

    cdaii - schema and data files added for r2dbc

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into customer(name) values ('Chakib');
+insert into customer(name) values ('Mohamed');
+insert into customer(name) values ('Chokri');
+insert into customer(name) values ('Anis');
+insert into customer(name) values ('Zakia');
+insert into customer(name) values ('Nouredine');
