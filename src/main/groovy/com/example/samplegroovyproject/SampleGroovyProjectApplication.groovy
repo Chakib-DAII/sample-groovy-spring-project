1eece299a4235012cb5df8f0fc55aa50180436a8
cdaii <chakibdaii1995@gmail.com>
2022-04-23 17:00:46 +0100

    cdaii - main class update

--- src/main/groovy/com/example/samplegroovyproject/SampleGroovyProjectApplication.groovy
+++ src/main/groovy/com/example/samplegroovyproject/SampleGroovyProjectApplication.groovy
@@ -1,35 +1,11 @@
 package com.example.samplegroovyproject
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr353.JSR353Module
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.actuate.endpoint.SanitizableData
-import org.springframework.boot.actuate.endpoint.SanitizingFunction
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.core.env.CommandLinePropertySource
 
 @SpringBootApplication
 class SampleGroovyProjectApplication {
 
-	@Bean
-	SanitizingFunction sanitizingFunction(){
-		return new SanitizingFunction() {
-			@Override
-			SanitizableData apply(SanitizableData data) {
-				return data.getPropertySource().getName() == CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME ?
-						data.withValue("not gonna get it") : data;
-			}
-		}
-	}
-
-	@Bean
-	ObjectMapper objectMapper() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.registerModule(new JSR353Module());
-
-		return objectMapper;
-	}
 	static void main(String[] args) {
 		SpringApplication.run SampleGroovyProjectApplication, args
 	}
